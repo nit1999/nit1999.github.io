7e6ec076d99e4060a730f9e8d1957eb2ae69dbba
Nitesh Kumar <https://github.com/nit1999/flappy-bird-2d>
2020-08-11 01:25:06 +0530

    file added

---
+++ index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nitesh Kumar</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+      <link rel="stylesheet" href="lightbox2/src/css/lightbox.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/fontawesome/css/fontawesome-all.min.css">
+<link rel = "icon" type = "image/png" href = "images/android-icon-192x192.png"/>
+   <!-- For apple devices -->
+   <link rel = "apple-touch-icon" type = "image/png" href = "images/apple-icon.png"/>
+
+</head>
+
+<body>
+<!--navigation bar----------->
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Nitesh Kumar</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#skill">Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#portfolio1" tabindex="-1" aria-disabled="true">Portfolio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact" tabindex="-1" aria-disabled="true">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<!----Social media-------->
+  <div id="home" class="carousel-inner" style="background-color: rgb(49, 54, 74);">
+    <div class="carousel-item active">
+      <img src="images/final.png" class="d-block w-100" alt="..." >
+      </div>
+    </div>
+<div class="linkinfo" style="  border-top:1px solid white;  border-bottom:1px solid white;">
+  <div class="container" style="margin:3px auto;text-align:center;">
+  <div class="d-inline social">
+    <a href="https://www.linkedin.com/in/nitesh-kumar-45bb9815a/"><i class="fab fa-linkedin-in" style="color:#0077B5;"></i></a>
+  </div>
+  <div class="d-inline social"><a href="https://www.facebook.com/Nitesh5292"><i class="fab fa-facebook-f" style="color:#0084ff;"></i></a></div>
+  <div class="d-inline social"><a href="https://www.instagram.com/nitesh_sharma5292/"><i class="fab fa-instagram" style="color:#e4405f;"></i></a></div>
+  <div class="d-inline social"><a href="https://github.com/nit1999/"><i class="fab fa-github-alt" style="color:#6e5494;"></i></a></div>
+</div>
+</div>
+<div class="bg-dark">
+  <br><br>
+<!----------------skills---------->
+<div id="skill" class="skills">
+  <h2 style="color:white;">Skills</h2>
+</div><br><br>
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">C++</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 55%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">55%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Java Core/Advanced</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 35%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">35%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Php</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 65%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">65%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Photoshop</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 75%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Illustrator</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 75%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Unity</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 65%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">65%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Html</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">90%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">Css</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 75%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">javascript</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
+      </div>
+    </div>
+  </div><br>
+  <div class="row">
+    <div class="col-6">
+      <span style="color:white;">MYSQL</span>
+    </div>
+    <div class="col-6">
+      <div class="progress bg-light border border-light">
+      <div class="progress-bar bg-dark" role="progressbar" style="width: 45%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">45%</div>
+      </div>
+    </div>
+  </div>
+</div>
+<br><br>
+</div>
+<!-- Portfolio---------->
+
+<div id="portfolio1" class="PortfolioTitle">
+  <br><br>
+<h2>Portfolio</h2>
+</div><br><br>
+<div class="container">
+<div class="row">
+<div class="col-md-4 col-12">
+<div class="card">
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="image/Screenshot_20190909_020845.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_020859.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_020937.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_020906.jpg" alt="Four slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  <!-- <img class="card-img-top" src="image/Screenshot_20190909_020859.jpg" alt="card img cap"> -->
+      <div class="card-body">
+        <h5 class="card-title">Flappy Bird 2D Dark Mode Game</h5>
+        <p class="card-text">Download and Play Flappy Bird <a href="Flappy Bird 2d dark.apk" download>
+        <br>  <button class="btn btn-dark btn-sm" name="button">download</button></a> </p>
+        <p class="card-text">If you want to improve this game then visit to my github<a href="https://github.com/nit1999/">
+        <br>  <button class="btn btn-success btn-sm" name="button">visit</button></a></p>
+    </div>
+</div>
+</div>
+<!-- ffas -->
+<div class="col-md-4 col-12">
+<div class="card">
+  <div id="carouselExampleControls1" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="image/Screenshot_20190909_020956.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_021000.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_021034.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_021040.jpg" alt="Four slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls1" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls1" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  <!-- <img class="card-img-top" src="image/Screenshot_20190909_020859.jpg" alt="card img cap"> -->
+      <div class="card-body">
+        <h5 class="card-title">Coin Collector 2D Game</h5>
+        <p class="card-text">Download and Play game <a href="CoinCount.apk" download>
+        <br>  <button class="btn btn-dark btn-sm" name="button">download</button></a></p>
+        <p class="card-text">If you want to improve this game then visit to my github<a href="https://github.com/nit1999/">
+        <br>  <button class="btn btn-success btn-sm" name="button">visit</button></a></p>
+    </div>
+</div>
+</div>
+
+
+<!-- ffas -->
+<div class="col-md-4 col-12">
+<div class="card">
+  <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="image/Screenshot_20190909_031230.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_031248.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_031252.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="image/Screenshot_20190909_031303.jpg" alt="Four slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  <!-- <img class="card-img-top" src="image/Screenshot_20190909_020859.jpg" alt="card img cap"> -->
+      <div class="card-body">
+        <h5 class="card-title">Flappy Bird 2D Light Mode Game</h5>
+        <p class="card-text">Download and Play Flappy Bird <a href="Flappy Bird 2d.apk" download><br>
+          <button class="btn btn-dark btn-sm" name="button">download</button></a></p>
+        <p class="card-text">If you want to improve this game then visit to my github<a href="https://github.com/nit1999/">
+          <br> <button class="btn btn-success btn-sm" name="button">visit</button></a></p>
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+<br>
+<br>
+<!---------contact me---->
+<div id="contact" class="contact">
+  <h2>Contact Me</h2>
+</div><br><br>
+<div class="container">
+  <form action="index.php" method="post">
+    <input type="text"  name="username" placeholder="Name" required="required"><br>
+    <input type="email"  name="email" placeholder="Email" required="required"><br>
+    <input type="number"  name="mobile" placeholder="Contact number" required="required"><br>
+    <textarea name="sms" rows="8" cols="80" placeholder="Message" required="required"></textarea><br><br>
+    <div class="submit">
+      <input type="submit" name="submit" value="send">
+    </div>
+  </form>
+</div>
+
+<br><br><br>
+<center>
+<div class="foot">
+<h5 style="color:white;padding-top:5px;">copyright &copy; Nitesh Kumar 2018</h5>
+</div>
+</center>
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
+<?php
+$con=mysqli_connect('localhost','root','','userdetail');
+if(isset($_POST['submit'])){
+  $name=$_POST["username"];
+  $email=$_POST["email"];
+  $mobile=$_POST["mobile"];
+  $sms=$_POST["sms"];
+  $sql="insert into data(name,email,mobileNo,Message) values('$name','$email','$mobile','$sms')";
+  $run=mysqli_query($con,$sql);
+if($run==TRUE){
+  ?>
+<script>
+  alert("thank you <?php echo $name; ?>");
+</script>
+  <?php
+}
+else
+  echo "Error !";
+}
+ ?>
